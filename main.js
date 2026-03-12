1ca9d70397edabdd307da4845a05129c67fec251
Mwotsam <kmwota@gmail.com>
2026-03-12 17:57:39 +0000

    Add fuctions

--- main.js
+++ main.js
@@ -22,3 +22,31 @@ let yearAt100 = currentYear + (100 - age)
 console.log("Age in days:", (ageInDays))
 console.log("Age in hours:", (ageInHours))
 console.log("Year At 100:", (yearAt100))
+
+
+// Functions
+function calculateArea(width, height) {
+    return width * height;
+}
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 5/9) + 32;
+}
+
+function isEven(number) {
+    return (number % 2 === 0);
+}
+
+function getInitials(fullName) {
+    const parts = fullName.split(" ");
+    let initials = "";
+
+    for(let part of parts) {
+        initials += parts[0].toUperCase();
+    }
+    return initials;
+}
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
