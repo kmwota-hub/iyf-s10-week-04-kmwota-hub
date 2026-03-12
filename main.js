8608938c173fd1ec55d325badc70811a6108bce9
Mwotsam <kmwota@gmail.com>
2026-03-12 09:37:58 +0000

    add age in days, age in hours, year at 100 and output console

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 // Variable Declaration
-let Name = "Shadrack Mwota";
-let Age = 22;
+let name = "Shadrack Mwota";
+let age = 22;
 let isStudent = true;
 let favouritecolours = ["blue","red","green"];
 let today = new Date();
 
 // Console Output
-console.log("My name is:", Name);
-console.log("My age is:", Age);
+console.log("My name is:", name);
+console.log("My age is:", age);
 console.log("Am I a student:", isStudent);
 console.log("My favourite colours are:", favouritecolours);
 console.log("Today's date is:", today);
+
+// Variable Declaration
+let ageInDays = age * 365;
+let ageInHours = ageInDays * 24;
+let currentYear = new Date().getFullYear();
+let yearAt100 = currentYear + (100 - age)
+
+// console
+console.log("Age in days:", (ageInDays))
+console.log("Age in hours:", (ageInHours))
+console.log("Year At 100:", (yearAt100))
