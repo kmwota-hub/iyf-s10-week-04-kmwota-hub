fc5b1fafcb1b9c1292437b7390d42ec464cf3781
Mwotsam <kmwota@gmail.com>
2026-03-25 07:02:58 +0000

    Add Mini project: calculator

--- main.js
+++ main.js
@@ -83,3 +83,46 @@ for (let i = 1; i <= rows; i++) {
     }
     console.log(stars);
 }
+
+// Mini-Project: Calculator
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Can not divide by zero"
+    }
+    return a / b;
+}
+
+function calculate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Invalid operator";
+    }
+}
+
+// Test
+console.log(calculate(10, "+", 5));  // 15
+console.log(calculate(10, "-", 5));  // 5
+console.log(calculate(10, "*", 5));  // 50
+console.log(calculate(10, "/", 5));  // 2
+console.log(calculate(10, "/", 0));  // Error message
